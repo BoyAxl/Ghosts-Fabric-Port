79073256e9f3f6496f25d0527f61c94a21b5620a
auribe <auribe@acl.cl>
2026-05-19 16:04:16 -0400

    ✨ Restore manual ghost glow layers

--- common/src/main/java/dev/xylonity/bonsai/ghosts/client/entity/layer/GhostGlowLayer.java
+++ common/src/main/java/dev/xylonity/bonsai/ghosts/client/entity/layer/GhostGlowLayer.java
@@ -1,21 +1,46 @@
 package dev.xylonity.bonsai.ghosts.client.entity.layer;
 
+import com.geckolib.constant.DataTickets;
 import com.geckolib.renderer.base.GeoRenderer;
-import com.geckolib.renderer.layer.builtin.AutoGlowingGeoLayer;
+import com.geckolib.renderer.base.RenderPassInfo;
+import com.geckolib.renderer.layer.GeoRenderLayer;
 import dev.xylonity.bonsai.ghosts.Ghosts;
 import dev.xylonity.bonsai.ghosts.common.entity.ghost.GhostEntity;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.rendertype.RenderTypes;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.Identifier;
 
-public class GhostGlowLayer extends AutoGlowingGeoLayer<GhostEntity, Void, EntityRenderState> {
+public class GhostGlowLayer extends GeoRenderLayer<GhostEntity, Void, EntityRenderState> {
+
+    private static final int FULL_BRIGHT = 15728880;
+    private static final int GLOW_COLOR = 0xFFFFFFFF;
+    private static final Identifier GLOW_TEXTURE = Ghosts.of("textures/entity/ghost_glowmask.png");
 
     public GhostGlowLayer(GeoRenderer<GhostEntity, Void, EntityRenderState> renderer) {
         super(renderer);
     }
 
     @Override
-    protected Identifier getTextureResource(EntityRenderState renderState) {
-        return Ghosts.of("textures/entity/ghost_glowmask.png");
+    public void submitRenderTask(RenderPassInfo<EntityRenderState> renderInfo, SubmitNodeCollector submitNodeCollector) {
+        if (!renderInfo.willRender()) {
+            return;
+        }
+
+        int previousColor = renderInfo.renderColor();
+        int previousLight = renderInfo.packedLight();
+        int previousOverlay = renderInfo.packedOverlay();
+
+        renderInfo.renderState().addGeckolibData(DataTickets.RENDER_COLOR, GLOW_COLOR);
+        renderInfo.renderState().addGeckolibData(DataTickets.PACKED_LIGHT, FULL_BRIGHT);
+        renderInfo.renderState().addGeckolibData(DataTickets.PACKED_OVERLAY, OverlayTexture.NO_OVERLAY);
+
+        getRenderer().submitRenderTasks(renderInfo, submitNodeCollector.order(1), RenderTypes.entityTranslucentEmissive(GLOW_TEXTURE));
+
+        renderInfo.renderState().addGeckolibData(DataTickets.RENDER_COLOR, previousColor);
+        renderInfo.renderState().addGeckolibData(DataTickets.PACKED_LIGHT, previousLight);
+        renderInfo.renderState().addGeckolibData(DataTickets.PACKED_OVERLAY, previousOverlay);
     }
 
 }
